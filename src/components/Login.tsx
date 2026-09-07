2f4e5f750064a3a78175a1817d416ff1db40ebd1
Deepthi <deepthimurthy.95@gmail.com>
2026-09-07 21:06:20 +0530

    implementing login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,29 +1,89 @@
 import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { HTMLFormMethod } from 'react-router-dom';
 
 const Login = () => {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({ username: '', password: '' });
+  const [formError, setFormError] = useState({ usernameInvalid: false, passwordInvalid: false });
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   const getUserName = ($event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(userName);
+    let name = $event.target.value;
+
+    setFormData((prev) => {
+      return { ...prev, username: name };
+    });
+
+    if (!emailRegex.test(formData.username)) {
+      console.log('Into else');
+      setFormError((prev) => {
+        return {
+          ...prev,
+          usernameInvalid: true,
+        };
+      });
+      console.log('Error::', formError.usernameInvalid);
+    }
   };
 
   const getPassword = ($event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(password);
+    let password = $event.target.value;
+    setFormData((prev) => {
+      return { ...prev, password: password };
+    });
   };
 
-  const signIn = () => {
-    console.log('UserName::', userName);
-    console.log('PassWord::', password);
+  const handleSubmit = ($event: React.FormEvent<HTMLFormElement>) => {
+    $event.preventDefault();
+    console.log('FormEvent::', formData);
   };
 
   return (
-    <form onSubmit={signIn}>
-      <input type="text" value={userName} onChange={getUserName} />
-      <input type="text" value={password} onChange={getPassword} />
-      <button type="submit">Sign In</button>
-      <span>First Time User? SignIn</span>
-    </form>
+    <>
+      <Paper
+        elevation={3}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: 300,
+        }}
+      >
+        <Box component="form" onSubmit={handleSubmit}>
+          <Stack spacing={2} sx={{ display: 'inline-flex' }}>
+            <TextField
+              required
+              id="username"
+              label="User Name"
+              type="email"
+              value={formData?.username}
+              onChange={getUserName}
+              error={formError.usernameInvalid}
+              helperText={formError.usernameInvalid ? 'Invalid UserName' : ''}
+            />
+            <TextField
+              required
+              id="password"
+              type="password"
+              label="Password"
+              value={formData?.password}
+              onChange={getPassword}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              disabled={formError.usernameInvalid || formError.passwordInvalid}
+            >
+              Sign In
+            </Button>
+          </Stack>
+        </Box>
+      </Paper>
+    </>
   );
 };
 export default Login;

3755b870c5bbbdc2af0e983981bac0fc2de1107f
Deepthi <deepthimurthy.95@gmail.com>
2026-09-07 19:36:29 +0530

    Implemented Login Template

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,29 @@
+import { useState } from 'react';
+
 const Login = () => {
-  return <h1>This is Login Page</h1>;
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const getUserName = ($event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(userName);
+  };
+
+  const getPassword = ($event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(password);
+  };
+
+  const signIn = () => {
+    console.log('UserName::', userName);
+    console.log('PassWord::', password);
+  };
+
+  return (
+    <form onSubmit={signIn}>
+      <input type="text" value={userName} onChange={getUserName} />
+      <input type="text" value={password} onChange={getPassword} />
+      <button type="submit">Sign In</button>
+      <span>First Time User? SignIn</span>
+    </form>
+  );
 };
 export default Login;

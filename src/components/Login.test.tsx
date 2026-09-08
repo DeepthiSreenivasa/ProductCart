3134deb1d559789e53314e31d1dff1a6e3a488cb
Deepthi <deepthimurthy.95@gmail.com>
2026-09-08 20:01:21 +0530

    added spy for console

--- src/components/Login.test.tsx
+++ src/components/Login.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Login from './Login';
 import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
+import { use } from 'react';
 
 describe('Login', () => {
   it('renders the login form', () => {
@@ -40,6 +41,27 @@ describe('Login', () => {
       username: 'Jhon',
       password: 'password',
     });
+  });
+
+  it('does not log console with passwords', async () => {
+    let user = userEvent.setup();
+
+    const consoleSpy = vi.spyOn(console, 'log');
+    render(<Login />);
+
+    let userNameField = screen.getByRole('textbox', { name: /user name/i });
+    await user.type(userNameField, 'Jhon');
+
+    let passwordField = screen.getByLabelText(/password/i);
+    await user.type(passwordField, 'password');
+
+    let submitButton = screen.getByRole('button', {
+      name: /sign in/i,
+    });
+    await user.click(submitButton);
+
+    expect(consoleSpy).not.toHaveBeenCalledWith(expect.stringContaining('Secret123'));
+    consoleSpy.mockRestore();
   });
 
   //Accessibility Tests
